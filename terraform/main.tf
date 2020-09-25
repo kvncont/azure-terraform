81d68772c8af806a0403b653c0e34159ce73e503
Kevin Contreras <kvncont@gmail.com>
2020-09-25 00:53:29 -0600

    Se dejan los valores default en el network_profile

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,6 +34,10 @@ resource azurerm_subnet subnet_aks {
   resource_group_name  = azurerm_resource_group.rg_aks.name
   virtual_network_name = var.vnet_aks_name
   address_prefixes     = var.subnet_aks_address
+
+  depends_on = [
+    azurerm_virtual_network.vnet_aks
+  ]
 }
 
 resource azurerm_log_analytics_workspace law_aks {
@@ -90,9 +94,9 @@ resource azurerm_kubernetes_cluster aks {
   network_profile {
     network_plugin     = "azure"
     load_balancer_sku  = "Standard"
-    service_cidr       = "10.0.0.0/16"
-    dns_service_ip     = "10.0.0.10"
-    docker_bridge_cidr = "172.17.0.1/16"
+    # service_cidr       = "10.0.0.0/16"
+    # dns_service_ip     = "10.0.0.10"
+    # docker_bridge_cidr = "172.17.0.1/16"
     outbound_type      = "loadBalancer"
   }
 
