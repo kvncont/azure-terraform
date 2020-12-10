1585df5805a09f5aece1caeb0b17ee2bd9521358
Kevin Contreras <kvncont@gmail.com>
2020-12-10 16:00:40 -0600

    Se eliminan las alertas

--- terraform/main.tf
+++ terraform/main.tf
@@ -139,6 +139,7 @@ resource azurerm_kubernetes_cluster aks {
   tags = var.tags
 }
 
+/*
 resource azurerm_monitor_action_group mag_aks_gsbd {
   name                = "GSBD"
   resource_group_name = azurerm_resource_group.rg_aks.name
@@ -221,6 +222,8 @@ resource azurerm_monitor_metric_alert mma_pod_status {
 
   tags = var.tags
 }
+
+*/
 
 # resource azurerm_monitor_diagnostic_setting ds_aks {
 #   name                       = var.diagnostic_setting_name
