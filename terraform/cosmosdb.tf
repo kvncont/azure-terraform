4a069ef79affd2964f269c89684228bbd6a72eea
Kevin Contreras <kvncont@gmail.com>
2021-07-25 05:45:24 -0600

    Prueba del environment Development IaC

--- terraform/cosmosdb.tf
+++ terraform/cosmosdb.tf
@@ -1,3 +1,27 @@
+# resource "azurerm_network_security_group" "nsg_terraform" {
+#   name                = "nsg-terraform"
+#   location            = azurerm_resource_group.rg_terraform.location
+#   resource_group_name = azurerm_resource_group.rg_terraform.name
+#   tags                = var.tags
+# }
+
+# resource "azurerm_virtual_network" "vnet_terraform" {
+#   name                = "vnet-terraform"
+#   location            = azurerm_resource_group.rg_terraform.location
+#   resource_group_name = azurerm_resource_group.rg_terraform.name
+#   address_space       = ["10.0.0.0/8"]
+#   # dns_servers         = var.vnet_aks_dns_server
+#   tags = var.tags
+# }
+
+# resource "azurerm_subnet" "subnet_terraform" {
+#   name                 = "subnet-terraform"
+#   resource_group_name  = azurerm_resource_group.rg_terraform.name
+#   virtual_network_name = azurerm_virtual_network.vnet_terraform.name
+#   address_prefixes     = ["10.100.100.0/24"]
+#   service_endpoints    = ["Microsoft.AzureCosmosDB"]
+# }
+
 resource "azurerm_cosmosdb_account" "cosmosdb_account_kvncont" {
   name                = "kvncont"
   location            = var.location
